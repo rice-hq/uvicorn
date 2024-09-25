cfd7ccf081e30cf043d9698390760eb11de963fe
Minsub Kim <rapidfsub@gmail.com>
2024-09-25 22:03:59 +0900

    Setup winloop automatically

--- uvicorn/loops/auto.py
+++ uvicorn/loops/auto.py
@@ -2,9 +2,16 @@ def auto_loop_setup(use_subprocess: bool = False) -> None:
     try:
         import uvloop  # noqa
     except ImportError:  # pragma: no cover
-        from uvicorn.loops.asyncio import asyncio_setup as loop_setup
 
-        loop_setup(use_subprocess=use_subprocess)
+        try:
+            import winloop
+        except ImportError:
+            from uvicorn.loops.asyncio import asyncio_setup as loop_setup
+            loop_setup(use_subprocess=use_subprocess)
+        else:
+            from uvicorn.loops.winloop import winloop_setup
+            winloop_setup(use_subprocess=use_subprocess)
+
     else:  # pragma: no cover
         from uvicorn.loops.uvloop import uvloop_setup
 
